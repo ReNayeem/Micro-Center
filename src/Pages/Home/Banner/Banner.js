da6ec3cb0f4e4711e3adb864814c628835204153
Nayeam Ahmed <renayeem@gmail.com>
2022-05-22 03:33:14 +0600

    banner done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -11,71 +11,65 @@ const Banner = () => {
     };
     return (
         <Carousel activeIndex={index} onSelect={handleSelect}>
-            <Carousel.Item>
+            <Carousel.Item className='banner-background'>
                 <div className='banner'>
                     <div className='banner2 container d-flex'>
                         <div className='first-order'>
-                            <h2 className='mb-3 banner-h2'>Natural & Organic</h2>
-                            <h1 className='product-name-text'><span className='h1-text-highlight-b1'>Bazaar!</span> <span className='h1-text-highlight-2'>Stock your daily morning market with us.</span></h1>
-                            <br />
-                            <p className='product-detail'></p>
+                            <h2 className='mb-3 banner-h2'>Great Selection of</h2>
+                            <hr />
+                            <h1 className='product-name-text text-center'><span className='h1-text-highlight-b1'>H-O-M-E!<br />APPLIANCES</span></h1>
+                            <hr />
+                            <p className='product-detail text-center'>Good Price & Best Deals</p>
                             <Link as={Link} to="/AllItems">
-                                <button className="banner-button mb-5">
-                                    <span className="hover-underline-animation"> Browse here </span>
-                                    <svg id="arrow-horizontal" xmlns="http://www.w3.org/2000/svg" width="30" height="10" viewBox="0 0 46 16">
-                                        <path id="Path_10" data-name="Path 10" d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" transform="translate(30)"></path>
-                                    </svg>
+                                <button className="banner-button">
+                                    BROWSE HERE
                                 </button>
                             </Link>
                         </div>
                         <div className='second-order'>
-                            <img className='product-image' src="Resources/bn4.png" alt="" />
+                            <img className='product-image' src="Resources/bn5.png" alt="" />
                         </div>
                     </div>
                 </div>
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item className='banner-background-2'>
                 <div className='banner'>
                     <div className='banner2 container d-flex'>
                         <div className='first-order'>
-                            <h2 className='mb-3 banner-h2'>Natural & Organic</h2>
-                            <h1 className='product-name-text'><span className='h1-text-highlight-b2'>Available?</span> <span className='h1-text-highlight-2'>All kinds of protein, and vitamin ingredients.</span></h1>
-                            <br />
-                            <p className='product-detail'></p>
+                            <h2 className='mb-3 banner-h2'>FIRST IS A NEW!?</h2>
+                            <hr />
+                            <h1 className='product-name-text text-center'><span className='h1-text-highlight-b1'>-FLAG-SHIP-<br />MODEL</span></h1>
+                            <hr />
+                            <p className='product-detail text-center'>INTERCHANGEABLE CAMERA WITH LENS</p>
                             <Link as={Link} to="/AllItems">
-                                <button className="banner-button mb-5">
-                                    <span className="hover-underline-animation"> Browse here </span>
-                                    <svg id="arrow-horizontal" xmlns="http://www.w3.org/2000/svg" width="30" height="10" viewBox="0 0 46 16">
-                                        <path id="Path_10" data-name="Path 10" d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" transform="translate(30)"></path>
-                                    </svg>
+                                <button className="banner-button">
+                                    BROWSE HERE
                                 </button>
                             </Link>
                         </div>
                         <div className='second-order'>
-                            <img className='product-image' src="Resources/bn3.png" alt="" />
+                            <img className='product-image' src="Resources/bn6.png" alt="" />
                         </div>
                     </div>
                 </div>
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item className='banner-background-3'>
                 <div className='banner'>
                     <div className='banner2 container d-flex'>
                         <div className='first-order'>
-                            <h2 className='mb-3 banner-h2'>Natural & Organic</h2>
-                            <h1 className='product-name-text'><span className='h1-text-highlight-b3'>Fruits!</span> <span className='h1-text-highlight-2'>Nothing can be compared when it comes to fruits.</span></h1>
-                            <br />
-                            <p className='product-detail'></p>
+                            <h2 className='mb-3 banner-h2'>Easier Than Connecting A Cable</h2>
+                            <hr />
+                            <h1 className='product-name-text text-center'><span className='h1-text-highlight-b1'>-SONY- WIRELESS<br />HEADPHONE</span></h1>
+                            <hr />
+                            <p className='product-detail text-center'>Enjoy All The Audio In Your Smartphone</p>
                             <Link as={Link} to="/AllItems">
-                                <button className="banner-button mb-5">
-                                    <span className="hover-underline-animation"> Browse here </span>
-                                    <svg id="arrow-horizontal" xmlns="http://www.w3.org/2000/svg" width="30" height="10" viewBox="0 0 46 16">
-                                        <path id="Path_10" data-name="Path 10" d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" transform="translate(30)"></path>
-                                    </svg>
+                                <button className="banner-button">
+                                    BROWSE HERE
                                 </button>
                             </Link>
                         </div>
                         <div className='second-order'>
-                            <img className='product-image' src="Resources/bn2.png" alt="" />
+                            <img className='product-image' src="Resources/bn7.png" alt="" />
                         </div>
                     </div>
                 </div>
