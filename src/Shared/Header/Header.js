c705f4f7023ea62a51874bb5ae1e77516e7350fe
Nayeam Ahmed <renayeem@gmail.com>
2022-05-22 00:45:31 +0600

    Logo added

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -3,6 +3,7 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 
 import { Link } from 'react-router-dom';
 import './Header.css'
+import logo from '../../../src/MicroCenter.png'
 
 const Header = () => {
     // const [user] = useAuthState(auth);
@@ -16,24 +17,24 @@ const Header = () => {
             <Navbar className='navbar' collapseOnSelect expand="lg" sticky='top'>
                 <Container>
                     <Navbar.Brand as={Link} to="/">
-                        Unnamed
+                        <img className='logo' src={logo} alt="" />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto navbar-link">
-                            <Nav.Link as={Link} to="/">Home</Nav.Link>
-                            <Nav.Link as={Link} to="/all-items">All Items</Nav.Link>
-                            <Nav.Link as={Link} to="blogs">Blogs</Nav.Link>
+                            <Nav.Link className='nav-link' as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link className='nav-link' as={Link} to="/all-items">All Items</Nav.Link>
+                            <Nav.Link className='nav-link' as={Link} to="blogs">Blogs</Nav.Link>
                         </Nav>
                         <Nav className='navbar-link'>
                             <>
 
-                                <Nav.Link as={Link} to="all-items">All Items</Nav.Link>
+                                <Nav.Link className='nav-link' as={Link} to="all-items">All Items</Nav.Link>
                             </>
 
 
 
-                            <Nav.Link as={Link} to="login">
+                            <Nav.Link className='nav-link' as={Link} to="login">
                                 Login
                             </Nav.Link>
                         </Nav>
